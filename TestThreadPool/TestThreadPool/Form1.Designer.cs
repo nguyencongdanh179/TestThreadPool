38aae9ea1ac269f1dae562a30845d358c51ce42e
nguyencongdanh179 <44129760+nguyencongdanh179@users.noreply.github.com>
2021-11-21 07:59:55 +0700

    Add files via upload

---
+++ TestThreadPool/TestThreadPool/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace TestThreadPool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbStatus = new System.Windows.Forms.RichTextBox();
+            this.lbInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rtbStatus
+            // 
+            this.rtbStatus.Location = new System.Drawing.Point(12, 53);
+            this.rtbStatus.Name = "rtbStatus";
+            this.rtbStatus.Size = new System.Drawing.Size(754, 283);
+            this.rtbStatus.TabIndex = 0;
+            this.rtbStatus.Text = "";
+            this.rtbStatus.TextChanged += new System.EventHandler(this.rtbStatus_TextChanged);
+            // 
+            // lbInfo
+            // 
+            this.lbInfo.AutoSize = true;
+            this.lbInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbInfo.Location = new System.Drawing.Point(301, 9);
+            this.lbInfo.Name = "lbInfo";
+            this.lbInfo.Size = new System.Drawing.Size(201, 25);
+            this.lbInfo.TabIndex = 1;
+            this.lbInfo.Text = "Status RichTextBox";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 352);
+            this.Controls.Add(this.lbInfo);
+            this.Controls.Add(this.rtbStatus);
+            this.Name = "Form1";
+            this.Text = "FormTest";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbStatus;
+        private System.Windows.Forms.Label lbInfo;
+    }
+}
+
---
+++ TestThreadPool/TestThreadPool/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestThreadPool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void CreateThread(object sender, EventArgs e)
+        {
+            WaitCallback callback = new WaitCallback(Dowork);
+            ThreadPool.QueueUserWorkItem(callback, "haha");
+        }
+        public void AppendTextBox(string value)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AppendTextBox), new object[] { value });
+                return;
+            }
+            rtbStatus.Text += value;
+        }
+        private void Dowork(Object obj)
+        {
+            Thread currentThread = Thread.CurrentThread;
+            string checkThreadPoolStatus = currentThread.IsThreadPoolThread.ToString();
+            string threadID = currentThread.ManagedThreadId.ToString();
+            for (int i = 0; i < 5; i++)
+            {
+                AppendTextBox("Count: "+ i + " -- ThreadID: " + threadID + "\n");
+                Thread.Sleep(2000);
+            }
+        }
+        private void MainJob(object sender, EventArgs e)
+        {
+            rtbStatus.Text = rtbStatus.Text + "OK \n \n";
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            timer.Interval = 2000;
+            timer.Tick += new EventHandler(CreateThread);
+            timer.Enabled = true;
+
+            System.Windows.Forms.Timer timerMain = new System.Windows.Forms.Timer();
+            timerMain.Interval = 2000;
+            timerMain.Tick += new EventHandler(MainJob);
+            timerMain.Enabled = true;
+        }
+
+        private void rtbStatus_TextChanged(object sender, EventArgs e)
+        {
+            rtbStatus.SelectionStart = rtbStatus.TextLength;
+            rtbStatus.ScrollToCaret();
+        }
+    }
+}
